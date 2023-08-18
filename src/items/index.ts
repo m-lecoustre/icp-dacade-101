840b88b61a9c6e8f19ad21c0b767d1762f939323
LECOUSTRE Maxime <MLECOUSTRE@nexity.fr>
2023-08-18 17:32:18 +0200

    adding Item structure CRUD

---
+++ src/items/index.ts
@@ -0,0 +1,73 @@
+import { $query, $update, Record, StableBTreeMap, Vec, match, Result, nat64, ic, Opt} from 'azle';
+import { v4 as uuidv4 } from 'uuid';
+
+/**
+ * This type represents an Item that is
+ *  standardizen object orderable and suppliable by many suppliers
+ */
+
+type Item = Record<{
+    id: string;
+    title:  string;
+    description:    string;
+    illustrationURL:    string;
+    createdAt:  nat64;
+    updatedAt:  Opt<nat64>;
+}>
+
+type ItemPayload = Record<{
+    title:  string;
+    description:    string;
+    illustrationURL:    string;
+}>
+const itemStorage = new StableBTreeMap<string, Item>(0, 44, 1024);
+
+$query;
+export function getTypes(): Result<Vec<Item>, string> {
+    return Result.Ok(itemStorage.values());
+}
+
+export function getType(id: string): Result<Item, string> {
+    return match(itemStorage.get(id), {
+        Some: (item) => Result.Ok<Item, string>(item),
+        None: () => Result.Err<Item, string>(`No item found with id=${id}`)
+    });
+}
+
+$update;
+export function addItem(payload: ItemPayload): Result<Item, string> {
+    const item: Item = { id: uuidv4(), createdAt: ic.time(), updatedAt: Opt.None, ...payload};
+    itemStorage.insert(item.id, item);
+    return Result.Ok<Item, string>(item);
+}
+
+$update;
+export function updateItem(id: string, payload: ItemPayload): Result<Item, string> {
+    return match(itemStorage.get(id), {
+        Some: (item) => {
+            const updatedItem: Item = {...item, ...payload, updatedAt: Opt.Some(ic.time())};
+            itemStorage.insert(item.id, updatedItem);
+            return Result.Ok<Item, string>(updatedItem);
+        },
+        None: () => Result.Err<Item, string>("can't update item cause there is no item with id=${id} found")
+    });
+}
+
+$update;
+export function deleteItem(id: string): Result<Item, string> {
+    return match(itemStorage.remove(id), {
+        Some: (item) => Result.Ok<Item, string>(item),
+        None: () => Result.Err<Item, string>(`can't delete item cause no item with id=${id} found`)
+    });
+}
+
+// a workaround to make uuid package work with Azle
+globalThis.crypto = {
+    "getRandomValues": () => {
+        let array = new Uint8Array(32);
+        for (let i = 0; i < array.length; i++) {
+            array[i] = Math.floor(Math.random() * 256);
+        }
+        return array;
+    }
+};
