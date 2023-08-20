a604a045a95d3725e2dbe77fe0fd7009adf90536
LECOUSTRE Maxime <MLECOUSTRE@nexity.fr>
2023-08-20 20:24:28 +0200

    revert

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,18 +14,3 @@ dfx canister call addItem '(record {"title"= "first Item"; "description"= "This 
 dfx canister call addItem '(record {"title"= "other Item"; "description"= "This is second item registered here"; "illusatrationURL"= "src" })';
 
 dfx canister call getItems '()';
-
-# creation of 4 suppliers
-
-dfx canister call suppliers addSupplier '(record: {"name"= "Supplier 1 })';
-
-dfx canister call suppliers addSupplier '(record {"name" = "Toto" })';
-
-dfx canister call suppliers addSupplier '(record {"name" = "Momo Supply" })';
-
-dfx canister call suppliers addSupplier '(record {"name" = "Titi" })';
-
-# Display all item & suppliers records
-
-dfx canister call items getItems '()';
-dfx canister call suppliers getSuppliers '()';
